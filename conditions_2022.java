e75bc8944a46fc4fd55ba3de1ea4d6c95a37c2ca
Umesh12345678 <91375812+Umesh12345678@users.noreply.github.com>
2022-01-16 12:32:25 +0530

    Create conditions_2022.java

---
+++ conditions_2022.java
@@ -0,0 +1,30 @@
+package FirstHand_Jan_14_2022;
+
+public class conditions_2022 {
+
+	    public static void main(String[]args){
+	        byte num1=23;
+	        byte num2=45;
+	        byte num3=24;
+
+	        if (num1>num2){
+	            if (num1>num3){
+	                System.out.println(num1);
+	            
+	            }
+	            else{
+	                System.out.println(num3);
+	            }
+	        }
+	        else {
+	            if (num2>num3){
+	                System.out.println(num2);
+	            
+	            }
+	            else {
+	                System.out.println(num3);
+	            }
+	        }
+	    }
+
+}
